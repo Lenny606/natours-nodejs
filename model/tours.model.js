1eaac5fcca5e09f729ad4339605001dd4f5c85f3
--help <you@example.com>
2024-11-10 19:54:45 +0100

    validations

--- model/tours.model.js
+++ model/tours.model.js
@@ -2,69 +2,82 @@ import mongoose from 'mongoose';
 import slugify from 'slugify';
 
 const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true, trim: true
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+            trim: true,
+            maxLength: [50, 'A tour name must have less than or equal to 50 characters'],
+            minLength: [10, 'A tour name must have more than or equal to 10 characters']
+        },
+        slug: String,
+        secretTours: {
+            type: Boolean,
+            default: false
+        },
+        duration: {
+            type: Number,
+            required: [true, 'A duration is missing'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A group size is missing'],
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A difficulty is missing'],
+            enum:
+                {
+                    values: ['easy', 'medium', 'difficult'],
+                    message: 'Difficulty must be easy, medium, or difficult'
+                }
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 4,
+            min: [1, 'Rating must be at least 1'],
+            max: [5, 'Rating must be at most 5']
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price'],
+        },
+        priceDiscount: Number,
+        summary: {
+            type: String,
+            required: [true, 'A tour must have a summary'],
+            trim: true
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have a image cover'],
+        },
+        images: [String],
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            select: false //excludes this field from returned documents
+        }, //or use timestamps: true in schema options
+        startDates: [Date]
     },
-    slug: String,
-    secretTours: {
-        type: Boolean,
-        default: false
-    },
-    duration: {
-        type: Number,
-        required: [true, 'A duration is missing'],
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A group size is missing'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A difficulty is missing'],
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price'],
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        required: [true, 'A tour must have a summary'],
-        trim: true
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a image cover'],
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false //excludes this field from returned documents
-    }, //or use timestamps: true in schema options
-    startDates: [Date]
-}, {
-    toJSON: {
-        virtuals: true
-    },
-    toObject: {
-        virtuals: true
+    {
+        toJSON: {
+            virtuals: true
+        }
+        ,
+        toObject: {
+            virtuals: true
+        }
     }
-})
+)
 //not part of database
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
