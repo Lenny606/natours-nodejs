62f92aadf0db862a9daaaef286dc5deff8882572
--help <you@example.com>
2024-11-10 15:19:14 +0100

    import function

---
+++ utils/import-dev-data.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import express from 'express';
+import dotenv from 'dotenv';
+import Tour from "../model/tours.model.js";
+import {connectDB} from "../db.js";
+
+//Setup
+dotenv.config();
+connectDB();
+const data = fs.readFileSync('./data/tours.json', 'utf-8');
+const parsedData = JSON.parse(data);
+
+//Import Data
+const importData = async () => {
+    try {
+        await Tour.create(parsedData); //accept array
+        console.log('Data Imported...');
+    } catch (error) {
+        console.error('Error importing data:', error);
+    } finally {
+        process.exit();
+    }
+}
+
+//Delete All Data
+
+const purgeCollection = async () => {
+    try {
+        await Tour.deleteMany(); //accept array
+        console.log('Data deleted...');
+
+    } catch (error) {
+        console.error('Error deleting data:', error);
+    } finally {
+        process.exit();
+    }
+}
+if (process.argv[2] === '--import') {
+    importData();
+
+} else if (process.argv[2] === '--purge') {
+    purgeCollection();
+}
+console.log(process.argv)
