8c32bcf9c0a352c56389be363922d684c3c95d5d
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-11-12 21:46:45 +0100

    prod / dev errors

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,11 +1,39 @@
-
 export const globalErrorHandler = (err, req, res, next) => {
-        err.status = err.status || "error"
-        err.statusCode = err.statusCode || 500
-        err.message = err.message || "Error"
+    err.status = err.status || "error"
+    err.statusCode = err.statusCode || 500
+    err.message = err.message || "Error"
 
+    if (process.env.NODE_ENV !== "production") {
+        sendErrorDev(err, res)
+    } else {
+        sendErrorProd(err, res)
+    }
+}
+
+function sendErrorProd(err, res) {
+    if (err.isOperational) {
         res.status(err.status).json({
             status: err.status,
-            message: err.message
+            error: err
+        })
+    } else {
+        //programing error - generic response
+        //log
+        console.error(err)
+        //send
+        res.status(500).json({
+            status: 'error',
+            message: "Something went wrong"
         })
     }
+
+}
+
+function sendErrorDev(err, res) {
+    res.status(err.status).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
