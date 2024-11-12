4122c238d49a6c5f9315525d1dff73a6b03a8df2
Lenny606 <92226943+Lenny606@users.noreply.github.com>
2024-11-12 22:25:30 +0100

    unhandled exceptions

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 import {connectDB} from "./db.js";
 import Tour from "./model/tours.model.js";
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message)
+    console.log('Uncaught Exception... Shutting down ...')
+    process.exit(1)
+
+})
 // const app = express();
 dotenv.config()
 
@@ -14,7 +20,7 @@ const server = app.listen(port, () => {
 //unhandled error outside of express (DB for example), shut down gracefully
 process.on('unhandledRejection', err => {
     console.log(err.name, err.message)
-    console.log('Unhandled rejection... Shuting down ...')
+    console.log('Unhandled rejection... Shutting down ...')
     server.close(() => {
         process.exit(1)
 
