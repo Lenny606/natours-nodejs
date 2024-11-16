3312385dcc51eb99f228a046ef2149de38e0ad8b
--help <you@example.com>
2024-11-16 19:01:40 +0100

    jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,26 @@
 import User from "../model/users.model.js";
 import {catchAsync} from "../utils/catchAsync.js";
+import jwt from "jsonwebtoken"
 
 export const signUpUser = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    const token = jwt.sign({
+            id: newUser._id
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRATION
+        }
+    )
     res.status(201).json({
         status: "success",
+        token,
         data: newUser,
     });
 });
