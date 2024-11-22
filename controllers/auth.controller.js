10ce59f2184eb51ce56813e677dd0cf6ac467b6f
--help <you@example.com>
2024-11-22 20:21:51 +0100

    cookie in response

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,6 +18,16 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
 
     const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRATION * 24 * 60 * 60 * 1000),
+        httpOnly: true, //only browser
+    }
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true //activate ssl
+    }
+    //remove pass from resp
+    user.password = undefined
+    res.cookie("jwt",)
 
     return res.status(statusCode).json({
         status: "success",
